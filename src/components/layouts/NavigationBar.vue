9de21debf586956b2c783914427f689b989e6d7b
Daniel Mendez <danielmenc@outlook.com>
2023-03-02 23:47:54 -0600

    #comment main page to manage all the components

---
+++ src/components/layouts/NavigationBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar
+    color="amber-lighten-1"
+  >
+    <v-app-bar-nav-icon variant="text"></v-app-bar-nav-icon>
+    
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+    
+    <v-toolbar-title>Shadai Shop</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn variant="text" icon="mdi-magnify"></v-btn>
+    <v-btn variant="text" icon="mdi-filter"></v-btn>
+    <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+  </v-app-bar>
+</template>
--- src/layouts/layout.vue
+++ src/layouts/layout.vue
@@ -1,22 +1,30 @@
 <template>
-  <v-card>
+  <main>
     <v-layout>
-      <v-app-bar
-        color="amber-lighten-1"
-      >
-        <v-app-bar-nav-icon variant="text"></v-app-bar-nav-icon>
-        
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        
-        <v-toolbar-title>Shadai Shop</v-toolbar-title>
-
-        <v-spacer></v-spacer>
-
-        <v-btn variant="text" icon="mdi-magnify"></v-btn>
-        <v-btn variant="text" icon="mdi-filter"></v-btn>
-        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-      </v-app-bar>
+      <Navigation />
     </v-layout>
-  </v-card>
+
+    <div class="mt-16 pa-2">
+      <Snackbar />
+      <Button />
+      <!-- <Background /> -->
+    </div>
+  </main>
+  <!-- <Navigation /> -->
+  <!-- <Background /> -->
 </template>
+<script>
+import Navigation from '@/components/layouts/NavigationBar.vue'
+import Background from '@/components/layouts/Background.vue'
+import Snackbar from '@/components/layouts/Snackbar.vue'
+import Button from '@/components/layouts/Button.vue'
+
+export default {
+  components: {
+    Navigation,
+    Background,
+    Snackbar,
+    Button
+  }
+}
+</script>
