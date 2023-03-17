cd5225e043027407686bc8a777be8f39b9bf99dd
Daniel Mendez <danielmenc@outlook.com>
2023-03-17 13:53:24 -0600

    #comment add child route to layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'layout',
-    component: () => import('../layouts/layout.vue')
+    component: () => import('../layouts/layout.vue'),
+    children: [
+      {
+        path: '/',
+        name: 'home',
+        component: () => import('../views/Home.vue')
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/AboutView.vue')
-  }
+
+  // route level code-splitting
+  // this generates a separate chunk (About.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   component: () => import('../views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
