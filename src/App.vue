3fadabf3cf720829fea3c30c6074d2aadb1f06cc
Daniel Mendez <danielmenc@outlook.com>
2023-03-17 20:17:30 -0600

    #comment add providers in app file

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
   <router-view />
 </template>
+<script>
+import providers from "./providers";
+
+export default {
+  name:"App",
+  data(){
+    return {
+    }
+  },
+  provide:providers
+}
+</script>
