255c268d1ae802d1e64890e9acd2808434264f36
Daniel Mendez <danielmenc@outlook.com>
2023-03-17 20:17:54 -0600

    #comment finish posts file

--- src/components/Home/Posts.vue
+++ src/components/Home/Posts.vue
@@ -5,7 +5,7 @@
   >
     <v-row no-gutters v-if="indexPair(i) == true" class="py-6">
       <v-col
-        class="pa-2"
+        class="mr-2"
         sm="5"
         md="6"
       >
@@ -14,7 +14,7 @@
             :src="post.img"
             lazy-src="/logo/shadai-main.jpeg"
             @click="redirect(post.id)"
-            height="300"
+            height="320"
             cover
             class="cursor-pointer"
           >
@@ -30,19 +30,121 @@
         </v-card>
       </v-col>
       <v-col class="text-left">
-        <p class="text-h3">{{ post.title }}</p>
-        <v-divider></v-divider>
-        <p class="text-subtitle-1">{{ post.description }}</p>
+        <v-hover>
+          <template v-slot:default="{ isHovering, props }">
+            <v-card
+              class="elevation-2 pa-3"
+              v-bind="props"
+              :color="isHovering ? 'light-green' : undefined"
+              height="320"
+            >
+              <v-card-title>
+                <p class="text-h3 pb-3">{{ post.title }}</p>
+                <v-row
+                  align="center"
+                  class="mx-0 pb-2"
+                >
+                  <v-rating
+                    :model-value="post.rating"
+                    color="amber"
+                    density="compact"
+                    half-increments
+                    readonly
+                    size="small"
+                  ></v-rating>
+  
+                  <div class="text-grey ms-4">
+                    4.5 (413)
+                  </div>
+                </v-row>
+              </v-card-title>
+              <v-divider></v-divider>
+              <v-card-text>
+                <p class="text-subtitle-1">{{ limitText(post.description) }}</p>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-list-item class="w-100">
+                  <template v-slot:append>
+                    <div class="justify-self-end">
+                      <v-btn icon>
+                        <v-badge :content="post.reaction" color="blue-lighten-5">
+                          <v-icon size="large" icon="mdi-heart" color="red" @click="addReaction(post)"></v-icon>
+                        </v-badge>
+                      </v-btn>
+                    </div>
+                    <v-btn icon>
+                      <v-badge :content="post.comments" color="blue-lighten-5">
+                        <v-icon size="large" icon="mdi-comment" color="blue" @click="redirect(post.id)"></v-icon>
+                      </v-badge>
+                    </v-btn>
+                  </template>
+                </v-list-item>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-hover>
       </v-col>
     </v-row>
+
     <v-row no-gutters v-else>
       <v-col class="text-right">
-        <p class="text-h3">{{ post.title }}</p>
-        <v-divider></v-divider>
-        <p class="text-subtitle-1">{{ post.description }}</p>
+        <v-hover>
+          <template v-slot:default="{ isHovering, props }">
+            <v-card
+              class="elevation-2 pa-3"
+              v-bind="props"
+              :color="isHovering ? 'light-green' : undefined"
+              height="320"
+            >
+              <v-card-title>
+                <p class="text-h3 pb-3">{{ post.title }}</p>
+                <v-row
+                  class="mx-0 pb-2 d-flex flex-row-reverse"
+                >
+                  <v-rating
+                    :model-value="post.rating"
+                    color="amber"
+                    density="compact"
+                    half-increments
+                    readonly
+                    size="small"
+                  ></v-rating>
+  
+                  <div class="text-grey ms-4">
+                    4.5 (413)
+                  </div>
+                </v-row>
+              </v-card-title>
+              <v-divider></v-divider>
+              <v-card-text>
+                <p class="text-subtitle-1">{{ limitText(post.description) }}</p>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-list-item class="w-100">
+                  <template v-slot:append>
+                    <div class="justify-self-end">
+                      <v-btn icon>
+                        <v-badge :content="post.reaction" color="blue-lighten-5">
+                          <v-icon size="large" icon="mdi-heart" color="red" @click="addReaction(post)"></v-icon>
+                        </v-badge>
+                      </v-btn>
+                    </div>
+                    <v-btn icon>
+                      <v-badge :content="post.comments" color="blue-lighten-5">
+                        <v-icon size="large" icon="mdi-comment" color="blue" @click="redirect(post.id)"></v-icon>
+                      </v-badge>
+                    </v-btn>
+                  </template>
+                </v-list-item>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-hover>
       </v-col>
       <v-col
-        class="pa-2"
+        class="ml-2"
         sm="5"
         md="6"
       >
@@ -51,7 +153,7 @@
             :src="post.img"
             lazy-src="/logo/shadai-main.jpeg"
             @click="redirect(post.id)"
-            height="300"
+            height="320"
             cover
             class="cursor-pointer"
           >
@@ -80,39 +182,56 @@
 </template>
 <script>
 export default {
+  inject:['strLimit'],
   data() {
     return {
+      chance: 1,
       page: 1,
       posts: [
         {
           id: 1,
           img: "/images/example.jpg",
           title: "tree of the life",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+          rating: 3.5,
+          reaction: 1,
+          comments: 0
         },
         {
           id: 2,
           img: "/images/example-1.jpg",
           title: "tree of the wather",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+          rating: 4,
+          reaction: 3,
+          comments: 10
         },
         {
           id: 3,
           img: "/images/example-2.jpeg",
           title: "someone lossing",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+          rating: 1,
+          reaction: 5,
+          comments: 74
         },
         {
           id: 4,
           img: null,
           title: "no image",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+          rating: 3.5,
+          reaction: 0,
+          comments: 6
         },
         {
           id: 5,
           img: null,
           title: "no image in other side",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+          rating: 4.5,
+          reaction: 10,
+          comments: 23
         },
       ]
     }
@@ -124,6 +243,20 @@ export default {
     },
     redirect(id) {
       console.log('redirecting to id...' + id)
+    },
+    async addReaction(post) {
+      // let react = await axios.post()
+
+      if (this.chance > 0) {
+        this.chance = 0
+        return post.reaction = post.reaction + 1
+      }
+
+      this.chance = 1
+      return post.reaction = post.reaction - 1
+    },
+    limitText(text) {
+      return this.strLimit(text, 270)
     }
   }
 }
