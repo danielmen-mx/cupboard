160e75b38e5841da5df19b6f5af6e1a7fcec3807
Daniel Mendez <danielmenc@outlook.com>
2023-03-21 11:19:11 -0600

    #comment add services for posts

---
+++ src/services/PostService.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+let url = 'http://127.0.0.1:8000/'
+
+export default {
+  index(params = []) {
+    return axios.get(url + 'api/posts', params)
+  },
+  store(data) {
+    return axios.post(url + 'api/posts', data)
+  }
+}
