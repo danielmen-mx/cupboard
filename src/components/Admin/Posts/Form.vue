3c1ed043f62992bbeb5764a0477d2e704af9c829
Daniel Mendez <danielmenc@outlook.com>
2023-03-21 11:18:32 -0600

    #comment add Admin component

---
+++ src/components/Admin/Posts/Form.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      width="1024"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">{{ title }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-text-field
+                  label="Nombre*"
+                  hint="Escribe el nombre de tu publicación"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-text-field
+                  label="Autor*"
+                  hint="Escribe el nombre del autor de la publicación"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*Campos requeridos</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue-darken-1"
+            variant="text"
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue-darken-1"
+            variant="text"
+            @click="dialog = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      title: "Añadir nueva publicación",
+      form: [
+
+      ]
+    }
+  },
+  mounted() {
+    this.emitter.on('openPostForm', () => {
+      this.dialog = true
+    })
+  },
+}
+</script>
