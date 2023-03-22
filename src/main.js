24de08f021b3472a57ccbb5ae0eda4380a0f4860
Daniel Mendez <danielmenc@outlook.com>
2023-03-21 22:27:23 -0600

    #comment import library to project

--- src/main.js
+++ src/main.js
@@ -9,10 +9,12 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import moment from 'moment';
 
 // Event handling
 import mitt from 'mitt'
 
+window.moment = moment
 const emitter = mitt()
 const vuetify = createVuetify({
   components,
