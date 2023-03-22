031b5e58c115bcac668a9ecc06d58b30d0606807
Daniel Mendez <danielmenc@outlook.com>
2023-03-22 14:01:51 -0600

    #comment complete table of posts

--- src/components/Admin/Posts.vue
+++ src/components/Admin/Posts.vue
@@ -10,55 +10,63 @@
     </v-btn>
   </div>
   <v-divider class="my-2"></v-divider>
-  <v-card color="red">
+  <v-card>
     <v-table
-      height="500px"
+      height="300px"
       fixed-header
-      class="elevation-1"
     >
       <thead>
-        <tr>
-          <th>
-            Nombre
-          </th>
-          <th>
-            Autor
-          </th>
-          <th>
-            Imagen
-          </th>
-          <th>
-            Tags
-          </th>
-          <th>
-            Creado el
-          </th>
-          <th>
-            Opciones
-          </th>
+        <tr class="text-subtitle-1">
+          <th>Nombre</th>
+          <th>Autor</th>
+          <th>Imagen</th>
+          <th>Tags</th>
+          <th>Creado el</th>
+          <th>Opciones</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="item in items" :key="item.name">
-          <td>{{ item.name }}</td>
-          <td>{{ item.autor }}</td>
-          <td>{{ item.image }}</td>
-          <td>{{ item.tags }}</td>
-          <td>{{ formatDate(item.created_at) }}</td>
           <td>
-            <v-menu offset-y>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn icon v-bind="attrs" v-on="on">
-                  <v-icon>mdi-dots-vertical</v-icon>
-                </v-btn>
+            <p class="text-subtitle-1">{{ item.name }}</p>
+          </td>
+          <td>
+            {{ item.autor }}
+          </td>
+          <td>
+            <a :href="item.image" class="text-blue-darken-4" target="_blank">
+              {{ item.image }}
+            </a>
+          </td>
+          <td>
+            <div class="d-flex justify-space-between">
+              <v-chip
+                v-for="(tag, i) in item.tags"
+                variant="outlined"
+                color="success"
+                label
+              >
+                <v-icon start icon="mdi-label"></v-icon>
+                {{ tag }}
+              </v-chip>
+            </div>
+          </td>
+          <td>
+            {{ formatDate(item.created_at) }}
+          </td>
+          <td class="text-center">
+            <v-menu>
+              <template v-slot:activator="{ props }">
+                <v-btn icon="mdi-dots-vertical" v-bind="props" variant="flat"></v-btn>
               </template>
-
               <v-list>
-                <v-list-item >
-                  <v-list-item-title>Edit</v-list-item-title>
-                </v-list-item>
-                <v-list-item >
-                  <v-list-item-title>Delete</v-list-item-title>
+                <v-list-item
+                  v-for="(action, index) in actions"
+                  :key="index"
+                >
+                  <v-list-item-title class="cursor-pointer">
+                    {{ action.title }}
+                  </v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-menu>
@@ -85,6 +93,10 @@ export default {
       apiService: PostService,
       itemsPerPage: 5,
       items: [],
+      actions: [
+        { title: 'Editar' },
+        { title: 'Eliminar' },
+      ],
     }
   },
   methods: {
@@ -93,16 +105,21 @@ export default {
         this.loading = true
 
         const resp = await this.apiService.index()
-        console.log(resp.data.data)
+
         this.items = resp.data.data
         this.emitter.emit('snackbarNotify', {color: 'success', message: resp.data.message})
         this.loading = false
       } catch (error) {
-        console.log(error);
+        console.log(error)
+        this.emitter.emit('snackbarNotify', {color: 'error', message: resp.data.error})
       }
     },
     openDialog() {
       this.emitter.emit('openPostForm')
+    },
+    separateTags(array) {
+      let json = JSON.stringify(array)
+      console.log(json)
     }
   },
   mounted() {
