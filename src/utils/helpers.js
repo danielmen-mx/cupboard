06dca1570323ce2e83c41f7a806211e66fba520b
Daniel Mendez <danielmenc@outlook.com>
2023-03-21 22:27:35 -0600

    #comment add some custom methods

---
+++ src/utils/helpers.js
@@ -0,0 +1,50 @@
+export function debounce(fn, delay) {
+  var timeoutID = null
+  return function () {
+    clearTimeout(timeoutID)
+    var args = arguments
+    var that = this
+    timeoutID = setTimeout(function () {
+      fn.apply(that, args)
+    }, delay)
+  }
+}
+
+export const moneyFormat = {
+  methods: {
+    moneyFormat(total) {
+      var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+      });
+
+      return formatter.format(total);
+    }
+  }
+}
+
+export const numberFormat = {
+  methods: {
+    numberFormat(number, decimal = 0) {
+      return new Intl.NumberFormat('en-US', { maximumFractionDigits: decimal, minimumFractionDigits: decimal }).format(number);
+    }
+  }
+}
+
+export const formatDate = {
+  methods: {
+    formatDate(date, format = 'MM/DD/YYYY') {
+      return window.moment(date).format(format)
+    }
+  }
+}
+
+export const ucFirst = {
+  methods: {
+    ucFirst(text) {
+      if (typeof text !== 'string') return ''
+      return text.charAt(0).toUpperCase() + text.slice(1);
+    }
+  }
+}
