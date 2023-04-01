33e0dc027c5d7214f7df377b70a297d73d5eebd6
Daniel Mendez <danielmenc@outlook.com>
2023-03-31 23:25:04 -0600

    #comment add show method in post service

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -1,21 +1,24 @@
 import axios from 'axios'
-let url = 'http://127.0.0.1:8000/'
+let url = 'http://127.0.0.1:8000/api/'
 
 export default {
   index(params = []) {
-    return axios.get(url + 'api/posts', params)
+    return axios.get(url + 'posts', params)
   },
   store(data) {
-    return axios.post(url + 'api/posts', data, {
+    return axios.post(url + 'posts', data, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
     })
   },
+  show(id) {
+    return axios.get(url + 'posts/' + id)
+  },
   update(id, data, headers = null) {
-    return axios.post(url + 'api/posts/' + id, data, headers)
+    return axios.post(url + 'posts/' + id, data, headers)
   },
   remove(id) {
-    return axios.delete(url + 'api/posts/' + id)
+    return axios.delete(url + 'posts/' + id)
   }
 }
