0ce951e352ba42ce3bb585298375ba85410b9fe3
Daniel Mendez <danielmenc@outlook.com>
2023-04-26 19:59:19 -0600

    #comment create login page

---
+++ src/pages/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-parallax
+    class="h-screen"
+    lazy-src="../../public/logo/shadai-main.jpeg"
+    v-resize="onResize"
+  >
+    <div class="d-flex fill-height justify-center align-center">
+      <v-card class="mx-auto px-6 py-8" :width="width">
+      <LoginForm />
+    </v-card>
+    </div>
+  </v-parallax>
+  <Register />
+</template>
+<script>
+import login from '@/components/Common/Responsives/login.vue'
+import Register from '../components/Login/Register.vue'
+import LoginForm from '../components/Login/LoginForm.vue'
+
+export default {
+    extends: login,
+    components: {
+      Register,
+      LoginForm
+    },
+    data: () => ({
+      //
+    }),
+    mounted() {
+      this.onResize()
+    }
+  }
+</script>
