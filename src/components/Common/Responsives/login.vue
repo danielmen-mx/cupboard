7f0271e10241f020201d623a3e2d1b8a51715de9
Daniel Mendez <danielmenc@outlook.com>
2023-04-26 19:59:48 -0600

    #comment create login responsive

---
+++ src/components/Common/Responsives/login.vue
@@ -0,0 +1,36 @@
+<template></template>
+<style scoped></style>
+<script>
+import Responsive from '../Responsive.vue'
+
+export default {
+  extends: Responsive,
+  data() {
+  return {
+      width: ''
+    }
+  },
+  methods: {
+    responsiveConfiguration() {
+      if (this.windowSize.width < 800) {
+        this.width = ''
+      }
+
+      if (this.windowSize.width > 800) {
+        this.width = '25%'
+      }
+    },
+  },
+  mounted() {
+    this.emitter.on('responsiveFit', () => {
+      // if (this.localResponsive == this.windowSize) return
+      // this.localResponsive = this.windowSize
+
+      this.$nextTick(() => {
+        this.responsiveConfiguration()
+        // console.log('Responsive login is working fine!')
+      })
+    })
+  },
+}
+</script>
