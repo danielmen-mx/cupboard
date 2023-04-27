aaaaae0fcefc60b98c264d1c79024bf802ec2d75
Daniel Mendez <danielmenc@outlook.com>
2023-04-26 19:57:15 -0600

    #comment comment console.log in responsive file

--- src/components/Common/Responsive.vue
+++ src/components/Common/Responsive.vue
@@ -10,7 +10,7 @@ export default {
     onResize() {
       this.windowSize = { height: window.innerHeight, width: window.innerWidth }
       this.emitter.emit('responsiveFit')
-      console.log('WindowSize is: ', this.windowSize)
+      // console.log('WindowSize is: ', this.windowSize)
     },
     adviceReload(component, data) {
       this.emitter.emit(component, data)
