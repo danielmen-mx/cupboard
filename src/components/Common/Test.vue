f3509fa8f69c2737edadd9165a9d047a71ce26ad
daniel <dmendez@webforcehq.com>
2023-05-19 21:38:42 -0600

    #comment change the name of the test component

--- src/components/Common/Test.vue
+++ src/components/Common/Test.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="skeleton-container skeleton-body skeleton-all">
+  <!-- <v-container class="skeleton-container skeleton-body skeleton-all">
     <div class="" id="card-link">
       <div class="skeleton skeleton-card-title"></div>
       <v-divider class="my-4"></v-divider>
@@ -10,33 +10,120 @@
       <div class="skeleton skeleton-card-right"></div>
       <div class="skeleton skeleton-card-left"></div>
       <div class="skeleton skeleton-card-right"></div>
-      <!-- <div class="card__header">
-        <div>
-          <div class="card__header header__img skeleton" id="logo-img" />
-        </div>
-        <div class="skeleton skeleton-text"></div>
-        <div class="skeleton skeleton-text"></div>
-      </div>
-  
-      <div class="card__body">
-        <div class="card__body body__text" id="card-details">
-          <div class="skeleton skeleton-text skeleton-text__body"></div> -->
-          <!-- <div class="skeleton skeleton-text skeleton-text__body"></div> -->
-        <!-- </div>
-  
-        <div class="card__body body__img">
-          <img class="skeleton" alt="" id="cover-img" />
-        </div>
-      </div>
-  
-      <div class="card__footer" id="card-footer">
-        <div class="skeleton skeleton-text skeleton-footer"></div>
-      </div> -->
     </div>
-  </v-container>
+  </v-container> -->
+  <v-data-table
+    v-model:items-per-page="itemsPerPage"
+    :headers="headers"
+    :items="desserts"
+    item-value="name"
+    class="elevation-1"
+  ></v-data-table>
 </template>
-<!-- <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script> -->
-<!-- <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script> -->
+<script>
+export default {
+  data() {
+    return {
+      itemsPerPage: 5,
+        headers: [
+          {
+            title: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            key: 'name',
+          },
+          { title: 'Calories', align: 'end', key: 'calories' },
+          { title: 'Fat (g)', align: 'end', key: 'fat' },
+          { title: 'Carbs (g)', align: 'end', key: 'carbs' },
+          { title: 'Protein (g)', align: 'end', key: 'protein' },
+          { title: 'Iron (%)', align: 'end', key: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22',
+          },
+        ],
+    }
+  },
+}
+</script>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap");
 .skeleton-all {
