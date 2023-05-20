17b6cfcdfdb242b1e544f3055bc54afdb2a02d2d
daniel <dmendez@webforcehq.com>
2023-05-19 21:39:42 -0600

    #comment improve home skeletomn

--- src/components/Common/Skeletons/HomeSkeleton.vue
+++ src/components/Common/Skeletons/HomeSkeleton.vue
@@ -1,27 +1,27 @@
 <template>
-  <v-container>
-    <v-sheet class="skeleton py-8 mx-16 rounded"></v-sheet>
+  <div>
+    <v-sheet class="skeleton py-8 mx-16 rounded" style="height: 10vh;"></v-sheet>
     <v-divider class="my-4"></v-divider>
     <div class="d-flex align-start flex-column">
-      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded"></v-sheet>
+      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded" style="height: 20vh;"></v-sheet>
     </div>
     <div class="d-flex align-end flex-column">
-      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded"></v-sheet>
+      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded" style="height: 20vh;"></v-sheet>
     </div>
     <div class="d-flex align-start flex-column">
-      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded"></v-sheet>
+      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded" style="height: 20vh;"></v-sheet>
     </div>
     <div class="d-flex align-end flex-column">
-      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded"></v-sheet>
+      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded" style="height: 20vh;"></v-sheet>
     </div>
-    <div class="d-flex align-start flex-column">
-      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded"></v-sheet>
+    <!-- <div class="d-flex align-start flex-column">
+      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded" style="height: 25vh;"></v-sheet>
     </div>
     <div class="d-flex align-end flex-column">
-      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded"></v-sheet>
+      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded" style="height: 25vh;"></v-sheet>
     </div>
     <div class="d-flex align-start flex-column">
-      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded"></v-sheet>
-    </div>
-  </v-container>
+      <v-sheet class="skeleton ma-2 pa-10 w-75 rounded" style="height: 25vh;"></v-sheet>
+    </div> -->
+  </div>
 </template>
