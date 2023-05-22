f825e19139f9c31d03a7439b501ba6a6e24c3ee7
daniel <dmendez@webforcehq.com>
2023-05-21 23:17:04 -0600

    #comment create new presentation, work in dashboards...

---
+++ src/components/Admin/Welcome.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card>
+    <v-card-item :title="`Bienvenido de nuevo, ` + user">
+      <template v-slot:subtitle>
+        <!-- <v-icon
+          icon="mdi-alert"
+          size="18"
+          color="error"
+          class="me-1 pb-1"
+        ></v-icon> -->
+
+        {{ date }}
+      </template>
+    </v-card-item>
+    <v-card-item>
+      
+    </v-card-item>
+  </v-card>
+</template>
+<script>
+import store from '../../store'
+import { getCurrentDate } from '../../utils/helpers'
+
+export default {
+  mixins: [getCurrentDate],
+  data() {
+    return {
+      user: 'user',
+      date: null
+    }
+  },
+  mounted() {
+    let user = store.getters['user']
+    this.date = this.getCurrentDate()
+
+    if (!user) return
+
+    this.user = user.fullname??user.username
+  },
+}
+</script>
