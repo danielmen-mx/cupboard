f5c2ae768b7fa905d407ae79c0de29cff4a43987
Daniel Mendez <danielmenc@outlook.com>
2023-07-12 19:38:27 -0600

    #comment create pagination responsive

---
+++ src/components/Common/Responsives/Admin/posts.vue
@@ -0,0 +1,26 @@
+<template></template>
+<style scoped></style>
+<script>
+import Responsive from '../Responsive.vue'
+
+export default {
+  extends: Responsive,
+  data() {
+    return {
+      //
+    }
+  },
+  methods: {
+    handle() {
+      // if (this.localResponsive == this.windowSize) return
+      // this.localResponsive = this.windowSize
+      this.$nextTick(() => {
+        //
+      })
+    },
+  },
+  mounted() {
+    this.listenEvent(this.event, this.handle)
+  },
+}
+</script>
