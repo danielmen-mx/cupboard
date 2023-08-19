4c222b5cdb2334af75a08c190ac3ca3dbb047e47
Daniel Mendez <danielmenc@outlook.com>
2023-08-19 00:11:13 -0600

    #comment improve pagination files to order the target of the pagination

---
+++ src/components/Common/Paginations/Admin.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="d-flex justify-end bg-grey-lighten-3">
+    <div class="d-flex">
+      <p class="pa-4">Items per page:</p>
+      <v-select
+        v-model="properties.items_per_page"
+        :items="['15', '25', '50']"
+        variant=""
+        :disabled="itemsPerPageDisabled"
+      ></v-select>
+    </div>
+    <p class="pa-4">{{ properties.first_item }} - {{ properties.last_item }} of {{ properties.total_items }}</p>
+    <div class="pa-2">
+      <v-btn
+        :disabled="backDisabled"
+        variant="text"
+        @click="movePosition(`--`)"
+      ><v-icon>mdi-arrow-left</v-icon></v-btn>
+      <v-btn
+        :disabled="forwardDisabled"
+        variant="text"
+        @click="movePosition(`++`)"
+      ><v-icon>mdi-arrow-right</v-icon></v-btn>
+    </div>
+  </div>
+</template>
+<script>
+// IMPORTANT: in the line 8 v-select > variant property is empty, keep as it is because there's no way to use the style we need
+import Pagination from '../Pagination.vue'
+
+export default {
+  extends: Pagination,
+  props: {
+    pagination_values: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      event: 'updatePaginationTable'
+    }
+  },
+  mounted() {
+    this.setProperties()
+  },
+  watch: {
+    'properties.items_per_page': {
+      handler: function () {
+        if (!this.properties.items_per_page) return
+        this.dispatchNewProperties(this.event)
+      },
+      deep: true,
+      immediate: true
+    },
+    'pageRequested': {
+      handler: function () {
+        if (!this.pageRequested || this.pageRequested === this.properties.current_page) return
+        this.dispatchNewProperties(this.event)
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/Common/Paginations/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-pagination
+    v-model="currentPage"
+    :length="this.lastPage"
+    prev-icon="mdi-menu-left"
+    next-icon="mdi-menu-right"
+    :total-visible="7"
+    rounded="circle"
+  ></v-pagination>
+</template>
+<script>
+import Pagination from '../Pagination.vue'
+
+export default {
+  extends: Pagination,
+  props: {
+    pagination_values: {
+      type: Object,
+      required: false
+    }
+  },
+  data() {
+    return {
+      event: 'updateHomePaginationTable'
+    }
+  },
+  mounted() {
+    this.setProperties()
+  },
+  watch: {
+    'currentPage': {
+      handler: function () {
+        if (this.currentPage === this.properties.current_page) return
+        this.dispatchNewProperties(this.event)
+      }
+    }
+  }
+}
+</script>
