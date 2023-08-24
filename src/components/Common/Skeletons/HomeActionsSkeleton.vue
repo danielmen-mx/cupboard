82855d7188e5f5c45345df4479e8acfea832767b
Daniel Mendez <danielmenc@outlook.com>
2023-08-23 23:59:35 -0600

    #comment create skeleton for new components

---
+++ src/components/Common/Skeletons/HomeActionsSkeleton.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="d-flex">
+    <v-avatar class="skeleton my-2 mr-2" size="45" height="5vh"></v-avatar>
+    <div class="d-flex flex-column justify-space-between">
+      <v-sheet class="skeleton rounded ma-1 pa-1 mt-2" height="2vh" width="7vw"></v-sheet>
+      <v-sheet class="skeleton rounded ma-1 pa-1 mb-2" height="2vh" width="3vw"></v-sheet>
+    </div>
+  </div>
+</template>
