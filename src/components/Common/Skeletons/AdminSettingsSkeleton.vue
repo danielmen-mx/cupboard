fda27528ed1ba7acee1516e489b31d1eab86a165
daniel <dmendez@webforcehq.com>
2023-09-26 11:22:46 -0600

    #comment create new skeleton loader for settings component

---
+++ src/components/Common/Skeletons/AdminSettingsSkeleton.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-sheet class="elevation-0 bg-grey-lighten-4 rounded-xl text-center text-grey-darken-3" height="68vh">
+    <div class="d-flex justify-space-between mx-4 pt-4">
+      <v-sheet class="skeleton rounded my-2" height="5vh" width="13vw"></v-sheet>
+    </div>
+    <v-divider></v-divider>
+    <v-responsive
+      class="my-4 mx-4 rounded"
+      max-width="35vw"
+    >
+      <v-sheet class="py-2">
+        <div class="d-flex justify-space-between my-4 mx-4">
+          <v-sheet class="skeleton rounded" height="5vh" width="17vw"></v-sheet>
+          <v-sheet class="skeleton rounded" height="5vh" width="12vw"></v-sheet>
+          <v-avatar class="skeleton" size="35" height="3vh"></v-avatar>
+        </div>
+        <div class="d-flex justify-space-between my-4 mx-4">
+          <v-sheet class="skeleton rounded" height="5vh" width="12vw"></v-sheet>
+          <v-sheet class="skeleton rounded" height="5vh" width="17vw"></v-sheet>
+          <v-avatar class="skeleton" size="35" height="3vh"></v-avatar>
+        </div>
+        <div class="d-flex justify-space-between my-4 mx-4">
+          <v-sheet class="skeleton rounded" height="5vh" width="14vw"></v-sheet>
+          <v-sheet class="skeleton rounded" height="5vh" width="18vw"></v-sheet>
+        </div>
+        <div class="d-flex justify-space-between my-4 mx-4">
+          <v-sheet class="skeleton rounded" height="5vh" width="12vw"></v-sheet>
+          <v-sheet class="skeleton rounded" height="5vh" width="20vw"></v-sheet>
+        </div>
+        <div class="d-flex justify-space-between my-4 mx-4">
+          <v-sheet class="skeleton rounded" height="5vh" width="14vw"></v-sheet>
+          <v-sheet class="skeleton rounded" height="5vh" width="18vw"></v-sheet>
+        </div>
+        <div class="d-flex align-end flex-column my-4 mx-4">
+          <v-sheet class="skeleton rounded" height="5vh" width="8vw"></v-sheet>
+        </div>
+      </v-sheet>
+    </v-responsive>
+  </v-sheet>
+</template>
