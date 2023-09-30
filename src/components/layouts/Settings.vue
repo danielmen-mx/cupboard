bc741db34edeeaf9626a214c0466f6a93a7774fb
Daniel Mendez <danielmenc@outlook.com>
2023-09-30 00:45:32 -0600

    #comment create new layout component for user settings component

---
+++ src/components/layouts/Settings.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      v-model="showModal"
+      activator="parent"
+      width="37vw"
+    >
+      <v-card>
+        <v-card-actions class="d-flex justify-space-between flex-row-reverse pa-2">
+          <v-btn
+          icon="mdi-close-circle-outline"
+          @click="showModal = false"
+          ></v-btn>
+          <v-card-title>
+            {{ translate("user_settings") }}
+          </v-card-title>
+        </v-card-actions>
+        <UserSettings />
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+import UserSettings from '../User/UserSettings.vue'
+
+export default {
+  components: {
+    UserSettings
+  },
+  data () {
+    return {
+      event: 'openUserSettings',
+      showModal: false
+    }
+  },
+  methods: {
+    handle() {
+      this.showModal = false
+      this.$nextTick(() => { this.showModal = true })
+    }
+  },
+  mounted() {
+    this.listenEvent(this.event, this.handle)
+  },
+}
+</script>
