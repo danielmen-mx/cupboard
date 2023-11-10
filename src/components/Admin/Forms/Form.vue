4fc4c68ac5191e39f8b19fe9e6576274f62b59da
Daniel Mendez <danielmenc@outlook.com>
2023-11-09 23:52:40 -0600

    #comment create new component about forms.... wip

---
+++ src/components/Admin/Forms/Form.vue
@@ -0,0 +1,21 @@
+<template></template>
+<script>
+export default {
+  data() {
+    return {
+      form: {},
+      loading: false,
+      apiService: null,
+      formComplete: false,
+      image_stored: null,
+      base_tags: [
+        this.translate("admin.posts.tag-list.release"),
+        this.translate("admin.posts.tag-list.update"),
+        this.translate("admin.posts.tag-list.overwrite")
+      ],
+      search: null,
+      event: null
+    }
+  },
+}
+</script>
