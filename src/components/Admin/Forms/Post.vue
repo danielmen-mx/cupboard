a0b9ef626e32580c35a7268b7dc5b792aafccf51
Daniel Mendez <danielmenc@outlook.com>
2023-11-15 00:33:28 -0600

    #comment improve post/form

--- src/components/Admin/Forms/Post.vue
+++ src/components/Admin/Forms/Post.vue
@@ -1,155 +1,197 @@
 <template>
-  <v-card color="transparent elevation-0 pa-2">
-    <!-- <v-form
-      v-model="formComplete"
-      ref="form"
-    > -->
-      <v-window v-model="step">
-        <v-window-item :value="1">
-          <v-card-text>
-            <v-row>
-              <v-col cols="12" sm="12" class="mb-2">
-                <v-text-field
-                  :loading="loading"
-                  v-model="form.name"
-                  density="compact"
-                  :rules="[required]"
-                  :label="translate('admin.posts.title')+'*'"
-                  :hint="translate('admin.posts.guides.title')"
-                  clearable
-                ></v-text-field>
-              </v-col>
-              <!--  -->
-            </v-row>
-
-            <quill-editor
-              v-model:content="form.description"
-              contentType="html"
-              :placeholder="translate('admin.posts.guides.description')+'*'"
-              style="height: 180px;"
-              class="mb-4"
-              theme="snow"
-              @keyup="checkFormComplete()"
-            ></quill-editor>
-
-            <v-row>
-              <v-col cols="12" sm="6">
-                <v-file-input
-                  prepend-icon="mdi-camera"
-                  accept="image/*"
-                  density="compact"
-                  show-size
-                  counter
-                  @change="addFile"
-                  :loading="loading"
-                  :label="getFileInputLabel()"
-                >
-                  <!-- <template v-if="image_stored" #prepend-inner>
-                    {{ image_stored.name }}
-                  </template> -->
-                </v-file-input>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-combobox
-                  v-model="form.tags"
-                  v-model:search="search"
-                  :hide-no-data="false"
-                  :items="base_tags"
-                  density="compact"
-                  hide-selected
-                  :hint="translate('admin.posts.guides.tags')"
-                  :label="translate('admin.posts.add-tags')"
-                  multiple
-                  persistent-hint
-                  chips
-                >
-                  <template v-slot:no-data>
-                    <v-list-item>
-                      <v-list-item-title>
-                        {{ translate("admin.posts.guides.new-tags-1") }} <kbd>{{ translate("enter") }}</kbd> {{ translate("admin.posts.guides.new-tags-2") }}
-                      </v-list-item-title>
-                    </v-list-item>
-                  </template>
-                </v-combobox>
-              </v-col>
-            </v-row>
-
-            <small>*{{ translate("validations.required-fields") }}</small>
-          </v-card-text>
-        </v-window-item>
-    
-        <v-window-item :value="2">
-          <v-card-text>
-            <v-row>
-              <v-col cols="12" sm="12">
-                <v-text-field
-                  :loading="loading"
-                  v-model="form.autor"
-                  density="compact"
-                  :rules="[required]"
-                  :label="translate('admin.posts.autor')+'*'"
-                  :hint="translate('admin.posts.guides.autor')"
-                  clearable
-                ></v-text-field>
-              </v-col>
-            </v-row>
-
-            <small>*{{ translate("validations.required-fields") }}</small>
-          </v-card-text>
-        </v-window-item>
-    
-        <v-window-item :value="3">
+  <v-card class="mx-auto pa-2 ma-2 elevation-0 bg-grey-lighten-5">
+    <template class="d-flex justify-space-between pa-2">
+      <v-card-title class="text-h6 font-weight-regular justify-space-between">
+        <v-avatar
+          color="blue-accent-2"
+          size="24"
+          v-text="step"
+        ></v-avatar>
+        <span class="pl-2">{{ currentTitle() }}</span>
+      </v-card-title>
+      <v-card-actions>
+        <v-btn
+          icon="mdi-close-circle-outline"
+          @click="closeDrawer()"
+        ></v-btn>
+      </v-card-actions>
+    </template>
+    <v-window v-model="step">
+      <!-- Step One -->
+      <v-window-item :value="1">
+        <v-card-text>
           <v-row>
-            <v-col cols="12" sm="6">
+            <v-col cols="12" sm="12" class="mb-2">
               <v-text-field
-                :disabled="true"
+                :loading="loading"
                 v-model="form.name"
+                density="compact"
+                :rules="[required]"
                 :label="translate('admin.posts.title')+'*'"
-                density="compact"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="6">
-              <v-text-field
-                :disabled="true"
-                v-model="form.autor"
-                :label="translate('admin.posts.autor')+'*'"
-                density="compact"
+                :hint="translate('admin.posts.guides.title')"
+                clearable
               ></v-text-field>
             </v-col>
           </v-row>
-          <v-text-field
-            :disabled="true"
-            v-model="form.description"
-            :label="translate('admin.posts.description')"
-            density="compact"
-          ></v-text-field>
+
+          <quill-editor
+            v-model:content="form.description"
+            contentType="html"
+            :placeholder="translate('admin.posts.guides.description')+'*'"
+            style="height: 180px;"
+            class="mb-4"
+            theme="snow"
+          ></quill-editor>
+
           <v-row>
             <v-col cols="12" sm="6">
-              <v-text-field
-                :disabled="true"
-                v-model="image_stored.name"
+              <v-file-input
                 prepend-icon="mdi-camera"
+                accept="image/*"
+                density="compact"
                 show-size
                 counter
-                density="compact"
+                @change="addFile"
+                :loading="loading"
                 :label="getFileInputLabel()"
-              ></v-text-field>
+              >
+                <!-- <template v-if="image_stored" #prepend-inner>
+                  {{ image_stored.name }}
+                </template> -->
+              </v-file-input>
             </v-col>
             <v-col cols="12" sm="6">
-              <v-text-field
-                :disabled="true"
-                v-model="form.autor"
-                :label="translate('admin.posts.add-tags')"
+              <v-combobox
+                v-model="form.tags"
+                v-model:search="search"
+                :hide-no-data="false"
+                :items="base_tags"
                 density="compact"
+                hide-selected
+                :hint="translate('admin.posts.guides.tags')"
+                :label="translate('admin.posts.add-tags')"
+                multiple
+                persistent-hint
+                chips
+              >
+                <template v-slot:no-data>
+                  <v-list-item>
+                    <v-list-item-title>
+                      {{ translate("admin.posts.guides.new-tags-1") }} <kbd>{{ translate("enter") }}</kbd> {{ translate("admin.posts.guides.new-tags-2") }}
+                    </v-list-item-title>
+                  </v-list-item>
+                </template>
+              </v-combobox>
+            </v-col>
+          </v-row>
+
+          <small>*{{ translate("validations.required-fields") }}</small>
+        </v-card-text>
+      </v-window-item>
+
+      <!-- Step Two -->
+      <v-window-item :value="2">
+        <v-card-text>
+          <v-row>
+            <v-col cols="12" sm="12">
+              <v-text-field
+                :loading="loading"
+                v-model="form.autor"
+                density="compact"
+                :rules="[required]"
+                :label="translate('admin.posts.autor')+'*'"
+                :hint="translate('admin.posts.guides.autor')"
+                clearable
               ></v-text-field>
             </v-col>
           </v-row>
-        </v-window-item>
-      </v-window>
-    <!-- </v-form> -->
+
+          <small>*{{ translate("validations.required-fields") }}</small>
+        </v-card-text>
+      </v-window-item>
+
+      <!-- Step three -->
+      <v-window-item :value="3">
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              :disabled="true"
+              v-model="form.name"
+              :label="translate('admin.posts.title')+'*'"
+              density="compact"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              :disabled="true"
+              v-model="form.autor"
+              :label="translate('admin.posts.autor')+'*'"
+              density="compact"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-text-field
+          :disabled="true"
+          v-model="form.description"
+          :label="translate('admin.posts.description')"
+          density="compact"
+        ></v-text-field>
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              :disabled="true"
+              prepend-icon="mdi-camera"
+              show-size
+              counter
+              density="compact"
+              :label="getFileInputLabel()"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-text-field
+              :disabled="true"
+              v-model="form.autor"
+              :label="translate('admin.posts.add-tags')"
+              density="compact"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-window-item>
+    </v-window>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-btn
+        v-if="step > 1"
+        variant="text"
+        @click="step--"
+      >
+        {{ translate("back") }}
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn
+        v-if="step < 3"
+        :disabled="!enableNextButton"
+        color="blue-darken-3"
+        variant="flat"
+        @click="stepForward()"
+      >
+        {{ translate("next") }}
+      </v-btn>
+      <v-btn
+        v-else
+        :disabled="!enableSubmit"
+        color="green"
+        variant="flat"
+        @click.prevent="submit"
+      >
+        {{ translate('create') }}
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 <script>
+import PostService from '@/services/PostService'
+import Form from '../../Common/Form.vue'
 import required from '../../Common/Form.vue'
 import TextEditor from '../../Common/TextEditor.vue'
 import { copyData, slugify } from '../../../utils/helpers'
@@ -157,28 +199,26 @@ import { imageHeaders } from '../../../services/api'
 
 export default {
   extends: {
-    //
+    Form
   },
   mixins: [required, copyData, slugify, imageHeaders],
   components: {
     TextEditor
   },
-  props: {
-    current_step: {
-      type: Number,
-      required: true
-    },
-    // form_parent: {
-    //   type: Object,
-    //   required: true
-    // }
-  },
   data() {
     return {
-      step: null,
+      apiService: PostService,
+      step: 1,
       enableNextButton: false,
-      form: {},
-      formComplete: false,
+      enableSubmit: false,
+      form: {
+        name: null,
+        autor: null,
+        description: null,
+        image: null,
+        tags: []
+      },
+      itemId: null,
       loading: false,
       image_stored: null,
       search: null,
@@ -187,68 +227,118 @@ export default {
         this.translate("admin.posts.tag-list.update"),
         this.translate("admin.posts.tag-list.overwrite")
       ],
+      event: 'updatePostAdminTable'
     }
   },
   methods: {
-    emitSubmit() {
-      this.$emit('enable_submit')
+    currentTitle() {
+      switch (this.step) {
+        case 1: return this.translate("basic_information")
+        case 2: return this.translate("autor_information")
+        default: return 'Preview'
+      }
+    },
+    stepForward() {
+      this.step++
+      this.enableNextButton = false
+    },
+    async submit() {
+      if (!this.validateStepOne() || !this.validateStepTwo()) return
+
+      try {
+        this.loading = true
+
+        let resp = null
+        if (!this.item_id) {
+          resp = await this.apiService.store(this.form)
+        } else {
+          let headers = this.getHeaders()
+          this.form._method = "PUT"
+          resp = await this.apiService.update(this.item_id, this.form, headers)
+        }
+
+        this.loading = false
+
+        this.$nextTick(() => {
+          this.closeDrawer()
+          this.successSnackbar(resp.data.message)
+          this.fireEvent(this.event)
+        })
+      } catch (error) {
+        console.log(error)
+        this.loading = false
+        this.$nextTick(() => {
+          this.errorSnackbar(error.response.data.message)
+        })
+      }
     },
     addFile(e) {
       this.form.image = e.target.files[0]
       this.image_stored = !this.form.image ? null : this.form.image
-      // if (!this.image_stored) return
-      if (this.image_stored) return
-      this.image_stored = null
     },
     getFileInputLabel() {
-      if (!this.image_stored) return this.translate("admin.posts.upload-asset")
-      return ''
+      if (!this.form.image) return this.translate("admin.posts.upload-asset")
+      return this.form.image.name
     },
     getHeaders() {
       if (this.form.image instanceof File) return this.imageHeaders()
       return null
-    },
-    checkFormComplete() {
-      if (this.form.name && this.form.autor && this.form.description) {
-        this.unlock = true
-      }
     },
     answeredForm(formTarget) {
       if (formTarget && formTarget.length > 1 && formTarget !== '') return true
       return false
     },
     validateStepOne() {
-      if (this.step !== 1) return
       if (this.answeredForm(this.form.name) && this.answeredForm(this.form.description)) return true
       return false
+    },
+    validateStepTwo() {
+      if (this.answeredForm(this.form.autor)) return true
+      return false
+    },
+    closeDrawer() {
+      this.step = 1
+      this.form.description = null
+      this.form = {
+        name: null,
+        autor: null,
+        image: null,
+        tags: null
+      }
+
+      this.$nextTick(() => { this.$router.push({ path: "/admin/posts" }) })
+
+      console.log(this.form)
     }
-  },
-  mounted() {
-    this.step = this.current_step
   },
   watch: {
     'form.name': {
       handler: function () {
-        console.log('form.name: ', this.form.name)
-        if (this.validateStepOne()) return this.$emit('enable_next_button', true)
-        return this.$emit('enable_next_button', false)
+        if (this.validateStepOne()) return this.enableNextButton = true
+        return this.enableNextButton = false
       },
       deep: true,
       immediate: true
     },
     'form.description': {
       handler: function () {
-        console.log('form.description: ', this.form.description)
-        if (this.validateStepOne()) return this.$emit('enable_next_button', true)
-        return this.$emit('enable_next_button', false)
+        if (this.validateStepOne()) return this.enableNextButton = true
+        return this.enableNextButton = false
       },
       deep: true,
       immediate: true
     },
-    'current_step': {
+    'form.autor': {
       handler: function () {
-        this.step = this.current_step
-        if (this.step === 1) return this.validateStepOne() ? this.$emit('enable_next_button', true) : this.$emit('enable_next_button', false)
+        if (this.validateStepTwo()) return this.enableNextButton = true
+        return this.enableNextButton = false
+      }
+    },
+    'step': {
+      handler: function () {
+        if (this.step === 1) return this.validateStepOne() ? this.enableNextButton = true : this.enableNextButton = false
+        if (this.step === 2) return this.validateStepTwo() ? this.enableNextButton = true : this.enableNextButton = false
+        if (this.step === 3) return this.validateStepOne() && this.validateStepTwo() ? this.enableSubmit = true : this.enableSubmit = false
       },
       deep: true,
       immediate: true
