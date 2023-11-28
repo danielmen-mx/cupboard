cc751828e9f078dd8a55ab47eaa4537f72ac7640
Daniel Mendez <danielmenc@outlook.com>
2023-11-28 00:43:25 -0600

    #comment prevent from propagation of event dispatched

--- src/components/Common/Confirmation.vue
+++ src/components/Common/Confirmation.vue
@@ -11,7 +11,7 @@
         <v-card-actions class="d-flex justify-space-around py-3">
           <v-btn
             color="red"
-            @click="confirm()"
+            @click.stop="confirm()"
             class="text-h6"
           >{{ translate("confirmations.yes") }}</v-btn>
           <v-btn
