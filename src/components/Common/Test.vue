e417478999ca3a79e500be624cdcb74949b4d4dd
Daniel Mendez <danielmenc@outlook.com>
2024-06-08 00:56:47 -0600

    #comment start building something amazing

--- src/components/Common/Test.vue
+++ src/components/Common/Test.vue
@@ -1,133 +1,29 @@
 <template>
-  <Button />
-  <v-row justify="center">
-    <v-navigation-drawer
-      v-model="drawer"
-      location="right"
-      width="700"
-      class="pa-2"
-      temporary
-    >
-      <v-card class="mx-auto pa-2 ma-2 elevation-0 bg-grey-lighten-3">
-        <template class="d-flex justify-space-between pa-2">
-          <v-card-title class="text-h6 font-weight-regular justify-space-between">
-            <span>{{ currentTitle }}</span>
-            <v-avatar
-              color="light-green"
-              size="24"
-              v-text="step"
-            ></v-avatar>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn
-              icon="mdi-close-circle-outline"
-              @click="closeDrawer()"
-            ></v-btn>
-          </v-card-actions>
-        </template>
-
-        <!-- <v-window v-model="step">
-          <v-window-item :value="1">
-            <v-card-text>
-              <v-text-field
-                label="Email"
-                placeholder="john@google.com"
-              ></v-text-field>
-              <span class="text-caption text-grey-darken-1">
-                This is the email you will use to login to your Vuetify account
-              </span>
-            </v-card-text>
-          </v-window-item>
-
-          <v-window-item :value="2">
-            <v-card-text>
-              <v-text-field
-                label="Password"
-                type="password"
-              ></v-text-field>
-              <v-text-field
-                label="Confirm Password"
-                type="password"
-              ></v-text-field>
-              <span class="text-caption text-grey-darken-1">
-                Please enter a password for your account
-              </span>
-            </v-card-text>
-          </v-window-item>
-
-          <v-window-item :value="3">
-            <div class="pa-4 text-center">
-              <v-img
-                class="mb-4"
-                contain
-                height="128"
-                src="https://cdn.vuetifyjs.com/images/logos/v.svg"
-              ></v-img>
-              <h3 class="text-h6 font-weight-light mb-2">
-                Welcome to Vuetify
-              </h3>
-              <span class="text-caption text-grey">Thanks for signing up!</span>
-            </div>
-          </v-window-item>
-        </v-window> -->
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-btn
-            v-if="step > 1"
-            variant="text"
-            @click="step--"
-          >
-            Back
-          </v-btn>
-          <v-spacer></v-spacer>
-          <v-btn
-            v-if="step < 3"
-            color="light-green"
-            variant="flat"
-            @click="step++"
-          >
-            Next
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-navigation-drawer>
-  </v-row>
+  <Item />
 </template>
 <script>
 import Button from '../layouts/Button.vue'
+import Item from '../../components/Store/Item.vue'
 
 export default {
   components: {
-    Button
+    Button,
+    Item
   },
   data: () => ({
     step: 1,
     drawer: false,
   }),
   computed: {
-    currentTitle () {
-      switch (this.step) {
-        case 1: return 'Sign-up'
-        case 2: return 'Create a password'
-        default: return 'Account created'
-      }
-    },
+
   },
   methods: {
-    openDrawer() {
-      return this.drawer = true
-    },
-    closeDrawer() {
-      this.$nextTick(() => { this.drawer = false })
-    }
+
   },
   mounted() {
-    this.listenEvent('openDrawer', this.openDrawer)
   },
   beforeDestroy() {
-    this.unlistenEvent('openDrawer', this.openDrawer)
+
   },
 }
 </script>
