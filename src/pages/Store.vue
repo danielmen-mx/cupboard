6a407bc66dcbbc56e6a46873e3c5f3a8bd1a57f5
Daniel Mendez <danielmenc@outlook.com>
2024-06-23 14:00:19 -0600

    #comment update Store page preview

--- src/pages/Store.vue
+++ src/pages/Store.vue
@@ -1,16 +1,57 @@
 <template>
   <v-card class="elevation-0">
-    <!-- <Item/> -->
+    <div class="d-flex mb-2">
+      <v-row dense>
+        <v-col cols="3">
+          <v-card
+            height="10vh"
+            elevation="0"
+            class="d-flex justify-center"
+          >
+            <v-card-title
+              class="align-self-center"
+            >Store</v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="9" class="d-flex align-center">
+          <v-card
+            color="secondary"
+            height="8vh"
+            width="100%"
+          >
+            <v-card-title>Offers component</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+
+    <v-divider></v-divider>
+
+    <div class="d-flex my-4">
+      <v-row dense>
+        <v-col cols="3">
+          <v-card
+            color="secondary"
+            height="75vh"
+          >
+            <v-card-title>Filters</v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="9" class="bg-grey">
+          <Items />
+        </v-col>
+      </v-row>
+    </div>
   </v-card>
 </template>
 <script>
 import Responsive from '@/components/Common/Responsive.vue'
-import Item from '../components/Store/Item.vue';
+import Items from '../components/Store/Items.vue';
 
 export default {
   extends: Responsive,
   components: {
-    Item
+    Items
   }
 }
 </script>
