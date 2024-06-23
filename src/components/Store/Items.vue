b16f48edde2182a658f034a5bbe8b7a052c92f33
Daniel Mendez <danielmenc@outlook.com>
2024-06-23 13:58:56 -0600

    #comment create Items component for Store page

---
+++ src/components/Store/Items.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- empty state -->
+  <!-- loading state -->
+  <v-card
+    class="mx-auto"
+    width="99%"
+    hover
+  >
+    <v-card-item>
+      <v-card-title>
+        Card title
+      </v-card-title>
+
+      <v-card-subtitle>
+        Card subtitle secondary text
+      </v-card-subtitle>
+    </v-card-item>
+
+    <v-card-text>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
