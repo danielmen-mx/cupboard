ff15eeb888d3c41a255ae45703e7e21c3b3aeb89
Daniel Mendez <danielmenc@outlook.com>
2024-07-02 23:34:21 -0600

    #comment create Store Item empty state component

---
+++ src/components/Common/EmptyState/StoreItemEmptyState.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card class="elevation-0 bg-grey-lighten-4 rounded-xl my-1 py-4 pl-10 text-center text-grey-darken-3">
+    <v-avatar color="grey-lighten-2" size="100">
+      <v-icon class="text-h2">
+        mdi-comment-remove-outline
+      </v-icon>
+    </v-avatar>
+    <p class="pt-6">
+      {{ translate("empty-states.Comments.no-comments") }}
+    </p>
+    <p class="text-h4">
+      {{ translate("empty-states.Comments.add-new") }}
+    </p>
+  </v-card>
+</template>
