ed0d4631e856e1206336683319f9d47b3e900115
Daniel Mendez <danielmenc@outlook.com>
2024-07-06 02:15:04 -0600

    #comment complete empty state for store items component

--- src/components/Common/EmptyState/StoreItemEmptyState.vue
+++ src/components/Common/EmptyState/StoreItemEmptyState.vue
@@ -1,5 +1,23 @@
 <template>
-  <v-card class="elevation-0 bg-grey-lighten-4 rounded-m my-1 py-4 text-center text-grey-darken-3">
-    {{ translate("empty-states.StoreItems.no-items") }}
+  <v-card class="d-flex flex-column align-center justify-center elevation-0" height="100%" style="padding-bottom: 18vh;">
+    <v-icon size="100">mdi-store-remove</v-icon>
+    <v-sheet class="elevation-0 pt-2 text-center text-grey-darken-3 text-h5">
+      Oh no!
+    </v-sheet>
+    <v-sheet class="elevation-0 text-center text-grey-darken-6 text-h5">
+      {{ translate("empty-states.StoreItems.no-items") }}
+    </v-sheet>
+    <v-sheet class="elevation-0 text-center text-grey-darken-1 text-h6">
+      {{ translate("empty-states.StoreItems.come-back") }} <strong class="cursor-pointer" @click="refreshComponent">{{ translate("empty-states.StoreItems.refresh").toLowerCase() }}</strong>
+    </v-sheet>
   </v-card>
 </template>
+<script>
+export default {
+  methods: {
+    refreshComponent() {
+      this.fireEvent("refresh-store-items-component")
+    }
+  },
+}
+</script>
