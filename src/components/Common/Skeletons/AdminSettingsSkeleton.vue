a4243ce52a9713061e31dee5672c200ebbaad4a5
Daniel Mendez <danielmenc@outlook.com>
2024-07-10 23:27:24 -0600

    #comment improve responsive design in admin settings skeleton

--- src/components/Common/Skeletons/AdminSettingsSkeleton.vue
+++ src/components/Common/Skeletons/AdminSettingsSkeleton.vue
@@ -1,38 +1,35 @@
 <template>
   <v-sheet class="elevation-0 bg-grey-lighten-4 rounded-xl text-center text-grey-darken-3" height="68vh">
     <div class="d-flex justify-space-between mx-4 pt-4">
-      <v-sheet class="skeleton rounded my-2" height="5vh" width="13vw"></v-sheet>
+      <v-sheet class="skeleton rounded my-2 mx-2" height="5vh" width="23vw"></v-sheet>
     </div>
     <v-divider></v-divider>
-    <v-responsive
-      class="my-4 mx-4 rounded"
-      max-width="35vw"
-    >
+    <v-responsive class="my-4 mx-4 rounded">
       <v-sheet class="py-2">
         <div class="d-flex justify-space-between my-4 mx-4">
-          <v-sheet class="skeleton rounded" height="5vh" width="17vw"></v-sheet>
-          <v-sheet class="skeleton rounded" height="5vh" width="12vw"></v-sheet>
-          <v-avatar class="skeleton" size="35" height="3vh"></v-avatar>
+          <v-sheet class="skeleton rounded mx-1" height="5vh" width="40vw"></v-sheet>
+          <v-sheet class="skeleton rounded mx-1" height="5vh" width="35vw"></v-sheet>
+          <v-avatar class="skeleton mx-1" size="45" height="3vh"></v-avatar>
         </div>
         <div class="d-flex justify-space-between my-4 mx-4">
-          <v-sheet class="skeleton rounded" height="5vh" width="12vw"></v-sheet>
-          <v-sheet class="skeleton rounded" height="5vh" width="17vw"></v-sheet>
-          <v-avatar class="skeleton" size="35" height="3vh"></v-avatar>
+          <v-sheet class="skeleton rounded mx-1" height="5vh" width="35vw"></v-sheet>
+          <v-sheet class="skeleton rounded mx-1" height="5vh" width="40vw"></v-sheet>
+          <v-avatar class="skeleton mx-1" size="45" height="3vh"></v-avatar>
         </div>
         <div class="d-flex justify-space-between my-4 mx-4">
-          <v-sheet class="skeleton rounded" height="5vh" width="14vw"></v-sheet>
-          <v-sheet class="skeleton rounded" height="5vh" width="18vw"></v-sheet>
+          <v-sheet class="skeleton rounded mx-1" height="5vh" width="25vw"></v-sheet>
+          <v-sheet class="skeleton rounded mx-1" height="5vh" width="55vw"></v-sheet>
         </div>
         <div class="d-flex justify-space-between my-4 mx-4">
-          <v-sheet class="skeleton rounded" height="5vh" width="12vw"></v-sheet>
-          <v-sheet class="skeleton rounded" height="5vh" width="20vw"></v-sheet>
+          <v-sheet class="skeleton rounded mx-1" height="5vh" width="20vw"></v-sheet>
+          <v-sheet class="skeleton rounded mx-1" height="5vh" width="60vw"></v-sheet>
         </div>
         <div class="d-flex justify-space-between my-4 mx-4">
-          <v-sheet class="skeleton rounded" height="5vh" width="14vw"></v-sheet>
-          <v-sheet class="skeleton rounded" height="5vh" width="18vw"></v-sheet>
+          <v-sheet class="skeleton rounded mx-1" height="5vh" width="30vw"></v-sheet>
+          <v-sheet class="skeleton rounded mx-1" height="5vh" width="50vw"></v-sheet>
         </div>
         <div class="d-flex align-end flex-column my-4 mx-4">
-          <v-sheet class="skeleton rounded" height="5vh" width="8vw"></v-sheet>
+          <v-sheet class="skeleton rounded mx-1" height="5vh" width="10vw"></v-sheet>
         </div>
       </v-sheet>
     </v-responsive>
