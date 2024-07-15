2cce51994259599d4a33b1d936f4b56bf02cd288
Daniel Mendez <danielmenc@outlook.com>
2024-07-14 21:08:22 -0600

    #comment create skeleton for single item component

---
+++ src/components/Common/Skeletons/StoreShowItemSkeleton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex">
+    <v-row dense>
+      <v-col cols="8">
+        <v-sheet class="skeleton rounded" style="height: 50vh;"></v-sheet>
+
+        <div class="my-2"></div>
+
+        <v-sheet class="skeleton rounded" style="height: 10vh;"></v-sheet>
+      </v-col>
+
+      <v-col cols="4">
+        <v-sheet class="skeleton rounded" style="height: 60.8vh;"></v-sheet>
+      </v-col>
+    </v-row>
+  </div>
+  <v-divider class="my-4"></v-divider>
+  
+  <v-sheet class="skeleton rounded" height="5vh"></v-sheet>
+</template>
