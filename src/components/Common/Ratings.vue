88ca3200ec9a7b635d2fc3288fba998bab11e585
Daniel Mendez <danielmenc@outlook.com>
2024-07-15 23:24:35 -0600

    #comment create ratings component

---
+++ src/components/Common/Ratings.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="d-flex align-center">
+    <v-rating
+      :model-value="rating.review"
+      readonly
+      color="amber"
+      size="small"
+    ></v-rating>
+    <p class="text-grey">
+      ({{ rating.review }} {{ translate("stars").toLowerCase() }})
+    </p>
+  </div>
+</template>
+<script>
+import RatingSkeleton from './Skeletons/RatingSkeleton.vue';
+
+export default {
+  components: { RatingSkeleton },
+  props: {
+    parent_rating: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      rating: {}
+    }
+  },
+  mounted() {
+    if (!this.parent_rating) return
+    this.rating = this.parent_rating
+  }
+}
+</script>
