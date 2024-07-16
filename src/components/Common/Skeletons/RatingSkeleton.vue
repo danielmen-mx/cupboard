53539e667a933f668375949d183c00db1433b614
Daniel Mendez <danielmenc@outlook.com>
2024-07-15 23:25:06 -0600

    #comment create rating skeleton for new component

---
+++ src/components/Common/Skeletons/RatingSkeleton.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="d-flex">
+    <div class="d-flex justify-space-between">
+      <v-sheet class="skeleton star"></v-sheet>
+      <v-sheet class="skeleton star"></v-sheet>
+      <v-sheet class="skeleton star"></v-sheet>
+      <v-sheet class="skeleton star"></v-sheet> 
+      <v-sheet class="skeleton star"></v-sheet> 
+    </div>
+    <v-sheet class="skeleton rounded mx-6" height="2vh" width="10vw"></v-sheet>
+  </div>
+</template>
