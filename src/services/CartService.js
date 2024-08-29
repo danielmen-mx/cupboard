18f93cc77d526222bf756df4fda60ec8207fd225
Daniel Mendez <danielmenc@outlook.com>
2024-08-29 02:21:26 -0600

    #comment create Cart Api Services

---
+++ src/services/CartService.js
@@ -0,0 +1,23 @@
+import Api from './api'
+
+export default {
+  index(params = []) {
+    return Api.get('/carts', params)
+  },
+  store(data) {
+    return Api.post('/carts', data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+  },
+  show(id) {
+    return Api.get('/carts/' + id)
+  },
+  update(id, data, headers = null) {
+    return Api.post('/carts/' + id, data, headers)
+  },
+  remove(id) {
+    return Api.delete('/carts/' + id)
+  }
+}
