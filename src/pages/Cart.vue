ecd6d7f58222514cda607185e88b421d66071fb8
Daniel Mendez <danielmenc@outlook.com>
2024-08-29 02:22:18 -0600

    #comment improve cart page

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,3 +1,25 @@
 <template>
-  cart page
+  <div class="d-flex">
+    <v-row dense>
+      <v-col cols="10" class="scroll-container-component" style="height: 70vh;">
+        <Items />
+      </v-col>
+    </v-row>
+    <v-row dense>
+      <v-col cols="2">
+        Something
+      </v-col>
+    </v-row>
+  </div>
 </template>
+<script>
+import Responsive from '@/components/Common/Responsive.vue'
+import Items from '../components/Cart/Items.vue';
+
+export default {
+  extends: Responsive,
+  components: {
+    Items
+  }
+}
+</script>
