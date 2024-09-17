72a011ff930fd8364c1649d571e5453f2da9a501
Daniel Mendez <danielmenc@outlook.com>
2024-09-16 23:09:03 -0600

    #comment create skeleton loader

---
+++ src/components/Common/Skeletons/CartItemsSkeleton.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-sheet class="skeleton rounded my-2 mx-2" height="10.65vh" width="55.5vw"></v-sheet>
+  <v-sheet class="skeleton rounded my-2 mx-2" height="10.65vh" width="55.5vw"></v-sheet>
+  <v-sheet class="skeleton rounded my-2 mx-2" height="10.65vh" width="55.5vw"></v-sheet>
+  <v-sheet class="skeleton rounded my-2 mx-2" height="10.65vh" width="55.5vw"></v-sheet>
+  <v-sheet class="skeleton rounded my-2 mx-2" height="10.65vh" width="55.5vw"></v-sheet>
+  <v-sheet class="skeleton rounded my-2 mx-2" height="10.65vh" width="55.5vw"></v-sheet>
+</template>
+
+<script>
+export default {
+  name: "ShoppingCartSkeleton",
+};
+</script>
+
+<style scoped>
+.v-skeleton-loader {
+  background-color: #f0f0f0;
+  opacity: 0.6;
+}
+</style>
