831885b4ebfa01b65ad2f02883c87ed7f747c567
Daniel Mendez <danielmenc@outlook.com>
2024-09-22 23:42:39 -0600

    #comment complete card sidebar component

--- src/components/layouts/CartSidebar.vue
+++ src/components/layouts/CartSidebar.vue
@@ -7,43 +7,68 @@
       width="400"
       app
     >
-      <v-toolbar flat dense>
-        <v-toolbar-title>Carrito de compras</v-toolbar-title>
+      <v-card class="d-flex align-center" flat>
+        <v-card-title>Carrito de compras</v-card-title>
         <v-spacer></v-spacer>
-        <v-btn icon @click="cartSidebar = false">
+        <v-btn
+          icon
+          @click="cartSidebar = false"
+          size="x-small"
+          flat
+        >
           <v-icon>mdi-close</v-icon>
         </v-btn>
-      </v-toolbar>
+      </v-card>
 
       <v-divider></v-divider>
 
-      <v-list dense>
-        <v-list-item v-for="(item, index) in cartItems" :key="index">
-          <v-img :src="item.image"></v-img>
+      <v-card
+        class="mx-auto"
+        max-width="500"
+        flat
+      >
+        <v-container fluid>
+          <v-row dense>
+            <v-col
+              v-for="item in items"
+              :key="item.title"
+              cols="12"
+            >
+              <v-card>
+                <v-img
+                  src=""
+                  lazy-src="/logo/shadai-main.jpeg"
+                  class="align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                  height="100px"
+                  cover
+                >
+                  <v-card-title class="text-white" :text="item.title"></v-card-title>
+                </v-img>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.name }}</v-list-item-title>
-            <v-list-item-subtitle>
-              Cantidad: {{ item.quantity }} | Precio: {{ item.price }}
-            </v-list-item-subtitle>
-          </v-list-item-content>
+                <div class="d-flex align-center justify-center ma-2">
+                  <span class="text-subtitle-1">Price example</span>
+                  <v-spacer></v-spacer>
+                  <div class="autocomplete-wrapper">
+                    <v-combobox density="compact" :items="[1,2,3,4,5]" class="no-border"></v-combobox>
+                  </div>
+                  <v-btn icon="mdi-trash-can" :ripple="false" flat></v-btn>
+                </div>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
 
-          <v-list-item-action>
-            <v-btn icon @click="removeItem(index)">
-              <v-icon>mdi-delete</v-icon>
-            </v-btn>
-          </v-list-item-action>
-        </v-list-item>
-      </v-list>
-
-      <v-divider></v-divider>
       <v-card flat>
         <v-card-text>
-          <div class="text-h6">Total: {{ total }}</div>
+          <div class="text-h6">MXN {{ moneyFormat(total) }}</div>
         </v-card-text>
-        <v-card-actions>
-          <v-btn color="success" block>Proceder al pago</v-btn>
-        </v-card-actions>
+
+        <div class="d-flex flex-column align-center justify-center ma-2">
+          <v-btn color="success" class="mb-2" block>Proceder al pago</v-btn>
+          <v-btn block>Ir al carrito</v-btn>
+        </div>
       </v-card>
     </v-navigation-drawer>
   </div>
@@ -51,19 +76,20 @@
 
 <script>
 export default {
+  inject: ['strLimit', 'moneyFormat'],
   data() {
     return {
       cartSidebar: false,
-      cartItems: [
+      items: [
         {
-          name: "Producto 1",
-          image: "https://via.placeholder.com/150",
+          title: "Producto 1",
+          src: "https://via.placeholder.com/150",
           quantity: 2,
           price: 350
         },
         {
-          name: "Producto 2",
-          image: "https://via.placeholder.com/150",
+          title: "Producto 2",
+          src: "https://via.placeholder.com/150",
           quantity: 1,
           price: 500
         },
@@ -72,12 +98,12 @@ export default {
   },
   computed: {
     total() {
-      return this.cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0);
+      return this.items.reduce((acc, item) => acc + item.quantity * item.price, 0);
     }
   },
   methods: {
     removeItem(index) {
-      this.cartItems.splice(index, 1);
+      this.items.splice(index, 1);
     },
     openCartSidebar() {
       this.cartSidebar = true
@@ -91,6 +117,21 @@ export default {
 
 <style scoped>
 .v-navigation-drawer {
-  background-color: white;
+  background-color: rgb(207, 207, 207);
+}
+
+.v-autocomplete {
+  width: 20px;
+}
+
+.autocomplete-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.no-border .v-input__control {
+  border: none !important;
+  box-shadow: none !important;
 }
 </style>
