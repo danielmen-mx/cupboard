88de8fa38bf488badc6c908956a398fb6b3175fb
Daniel Mendez <danielmenc@outlook.com>
2024-09-26 23:31:30 -0600

    #comment create component to get & fill the neccesary data to update cart state

---
+++ src/components/Cart/RemoveCartSidebarItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-btn icon="mdi-trash-can" :ripple="false" flat @click="updateState('cancelled')"></v-btn>
+</template>
+<script>
+import Actions from './Common/Actions.vue';
+
+export default {
+  extends: Actions,
+  props: {
+    parent_item_form: {
+      type: Object,
+      required: true
+    },
+  },
+  data() {
+    return {
+      form: {},
+      itemId: null,
+    }
+  },
+  mounted() {
+    if (!this.parent_item_form) return
+    this.itemId = this.parent_item_form.id
+    this.form = {
+      status: this.parent_item_form.status,
+      quantity: this.parent_item_form.quantity
+    }
+  },
+}
+</script>
