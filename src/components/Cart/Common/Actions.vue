edc87537a582e82082b54c486947da4b1a125de8
Daniel Mendez <danielmenc@outlook.com>
2024-09-26 23:32:28 -0600

    #comment create a new file containing the methods used for the actions

---
+++ src/components/Cart/Common/Actions.vue
@@ -0,0 +1,26 @@
+<template></template>
+<script>
+import Form from '../../Common/Form.vue';
+import CartService from '../../../services/CartService';
+
+export default {
+  extends: Form,
+  data() {
+    return {
+      form: {},
+      itemId: null,
+      apiService: CartService,
+      preventSnackbar: true
+    }
+  },
+  methods: {
+    updateState(newState) {
+      this.form.status = newState
+      this.submit()
+    },
+    successCallBack(resp) {
+      this.fireEvent("update-cart-table", resp.id)
+    }
+  }
+}
+</script>
