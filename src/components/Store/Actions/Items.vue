f7275c9d643f468cd7442d87bf705d6f666fe7e2
Daniel Mendez <danielmenc@outlook.com>
2024-09-29 23:54:46 -0600

    #comment create new component to get & set behavior of add cart buttons

---
+++ src/components/Store/Actions/Items.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-if="loading"></div>
+  <div v-else class="d-flex flex-row-reverse mx-2 px-2 pt-3">
+    <v-btn
+      :loading="loading"
+      class="mx-2 text-white"
+      color="light-green"
+      density="comfortable"
+      @click=""
+    >{{ translate("buy-now") }} {{ moneyFormat(itemPrice) }}</v-btn>
+    <v-btn
+      :loading="loading"
+      color="blue-darken-1"
+      variant="tonal"
+      density="comfortable"
+      @click="submit"
+    >{{ translate("add-cart") }}</v-btn>
+  </div>
+</template>
+<script>
+import CartService from '../../../services/CartService';
+import Actions from '../Common/Actions.vue';
+
+export default {
+  extends: Actions,
+  inject: ['moneyFormat'],
+  props: {
+    parent_item: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      apiService: CartService,
+      preventSnackbar: true,
+      itemPrice: 0.00,
+      form: {}
+    }
+  },
+  mounted() {
+    if (!this.parent_item) return
+    this.itemPrice = this.parent_item.price
+    this.setData()
+  },
+}
+</script>
