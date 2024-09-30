f1c9deb0a3a0f0e2eb74b5cb1e7ec4d669d23a32
Daniel Mendez <danielmenc@outlook.com>
2024-09-29 23:55:05 -0600

    #comment centralize methods of the "add cart" button

---
+++ src/components/Store/Common/Actions.vue
@@ -0,0 +1,24 @@
+<template></template>
+<script>
+import Form from '../../Common/Form.vue';
+import store from '../../../store';
+
+export default {
+  extends: Form,
+  methods: {
+    setData() {
+      let user = store.getters['user']
+      this.form.user_id = user.id
+      this.form.product_id = this.parent_item.id
+      this.form.status = "standby"
+      this.form.quantity = !this.quantity_parent ? 1 : this.quantity_parent
+    },
+    successCallBack(resp) {
+      this.fireEvent("open-sidebar-cart")
+    }
+  },
+  mounted() {
+
+  }  
+}
+</script>
