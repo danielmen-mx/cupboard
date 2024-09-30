9c31049c40c2271c93943d3f8f7444bd7abde56e
Daniel Mendez <danielmenc@outlook.com>
2024-09-30 00:17:47 -0600

    #comment add new quantity default data

--- src/components/Store/Common/Actions.vue
+++ src/components/Store/Common/Actions.vue
@@ -5,13 +5,18 @@ import store from '../../../store';
 
 export default {
   extends: Form,
+  data() {
+    return {
+      quantityRequired: 1
+    }
+  },
   methods: {
     setData() {
       let user = store.getters['user']
       this.form.user_id = user.id
       this.form.product_id = this.parent_item.id
       this.form.status = "standby"
-      this.form.quantity = !this.quantity_parent ? 1 : this.quantity_parent
+      this.form.quantity = this.quantityRequired
     },
     successCallBack(resp) {
       this.fireEvent("open-sidebar-cart")
