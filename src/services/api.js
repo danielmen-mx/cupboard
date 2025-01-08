6c69f4af72afe29c203d149c2c7099720a04a770
daniel mendez <danielmenc@outlook.com>
2025-01-07 22:37:22 -0600

    #comment add link to our webpage online

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import Cookies from "js-cookie"
 
 const baseURL = "http://127.0.0.1:8000/api"
 // const baseURL = "/api"
+// webunderdevelopment.com
 
 var headers = {
   "Content-Type": "application/json",
