5817d99ea18158396661dc3416f2f86e66c534ff
daniel mendez <danielmenc@outlook.com>
2025-01-10 00:01:30 -0600

    #wip...

--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -5,8 +5,19 @@
 export default {
   data() {
     return {
-      //
+      modelId: null,
     }
+  },
+  mounted() {
+    // TODO: check it up Sprint Cart Checkout && Single Item Checkout to finish module
+    // you receive an Id from $route, this id is knowed as modelId,
+    // the modelId represent user id or item (product) id
+    // add a way to identify wich one is received with the string "identifier"
+    // if the string "identifier" exists, the modelId is a user id
+    // use this to call the API wich responds with the current record, a complete cart, or a single item
+    // when you get this stop && take a rest, continue thinking about next steps
+    console.log(this.$route.params)
+    this.modelId = this.$route.params.id
   },
 }
 </script>
