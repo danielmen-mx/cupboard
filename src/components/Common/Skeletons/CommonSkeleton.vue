bc7d3a37f322dccbab4ba100c539b5a878359a46
daniel mendez <danielmenc@outlook.com>
2025-03-06 01:15:50 -0600

    #CommonSkeletonLoaders comment complete baseSkeletonLoader component that should be used for all new skeleton loaders

--- src/components/Common/Skeletons/CommonSkeleton.vue
+++ src/components/Common/Skeletons/CommonSkeleton.vue
@@ -1,4 +1,57 @@
-<template>Skeleton loader comming soon</template>
+<template>
+  <div v-if="type === 'card'">
+    <v-card>
+      <TitleLoader height="15vh" />
+      <div class="d-flex justify-center justify-space-around align-center">
+        <OneSingleCircleLoader size="55"/>
+        <OneSingleLineLoader width="75%" />
+      </div>
+    </v-card>
+  </div>
+  <div v-if="type === 'list'">
+    <v-card>
+      <v-list>
+        <v-list-item>
+          <TitleLoader height="5vh" />
+          <v-divider class="my-2"></v-divider>
+        </v-list-item>
+        <v-list-item v-for="index in 4" :key="'two-line-' + index">
+          <TwoLineLoader />
+        </v-list-item>
+        <v-list-item>
+          <OneSingleLineLoader />
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </div>
+  <div v-if="type === 'table'">
+    <v-card class="py-2">
+      <div class="d-flex justify-center justify-space-around align-center">
+        <TitleLoader v-for="index in 5" :key="index" :width="'15%'" />
+      </div>
+      <v-divider class="my-2"></v-divider>
+      <div v-for="index in 6" :key="index" class="d-flex justify-space-around justify-left align-center">
+        <OneSingleCircleLoader size="35"/>
+        <OneSingleLineLoader width="75%" />
+      </div>
+    </v-card>
+  </div>
+  <div v-if="type === 'gateway'">
+    <v-card>
+      <v-list>
+        <v-list-item>
+          <TitleLoader height="5vh"/>
+        </v-list-item>
+        <v-list-item v-for="index in 4" :key="'two-line-' + index">
+          <OneSingleLineLoader height="3vh" />
+        </v-list-item>
+        <v-list-item>
+          <OneSingleLineLoader height="4vh" class="my-4" />
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </div>
+</template>
 <script>
 import OneSingleLineLoader from './Common/OneSingleLineLoader.vue';
 import OneSingleCircleLoader from './Common/OneSingleCircleLoader.vue';
@@ -6,6 +59,13 @@ import TwoLineLoader from './Common/TwoLineLoader.vue';
 import TitleLoader from './Common/TitleLoader.vue';
 
 export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+      default: "card"
+    }
+  },
   components: {
     OneSingleLineLoader,
     OneSingleCircleLoader,
@@ -22,3 +82,11 @@ export default {
   },
 }
 </script>
+<style scoped>
+.centered-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+}
+</style>
