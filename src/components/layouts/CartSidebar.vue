1f052a4b449ed8da4f960bc424f437adf8707cb3
daniel mendez <danielmenc@outlook.com>
2025-09-12 00:45:05 -0600

    #commit complete Cart Sidebar

--- src/components/layouts/CartSidebar.vue
+++ src/components/layouts/CartSidebar.vue
@@ -1,16 +1,14 @@
 <template>
-  <div>
-    <v-navigation-drawer
-      v-model="cartSidebar"
-      location="right"
-      temporary
-      width="400"
-      app
-    >
-
-      <v-card class="d-flex align-center" flat>
-        <v-card-title>Carrito de compras</v-card-title>
-        <v-spacer></v-spacer>
+  <v-navigation-drawer
+    v-model="cartSidebar"
+    location="right"
+    temporary
+    width="400"
+    app
+  >
+    <v-card class="d-flex flex-column" flat>
+      <v-card-title class="d-flex align-center justify-space-between">
+        <span class="text-h6">{{ translate("cart") }}</span>
         <v-btn
           icon
           @click="closeCartSidebar()"
@@ -19,72 +17,97 @@
         >
           <v-icon>mdi-close</v-icon>
         </v-btn>
-      </v-card>
+      </v-card-title>
+    </v-card>
+    <v-divider></v-divider>
+    <template v-if="!items">
+      <!-- Skeleton Loader -->
+    </template>
+    <div v-else>
+      <template v-if="!items.length">
+        <v-container class="d-flex flex-column align-center justify-center text-center fill-height">
+          <v-img
+            src="https://cdn-icons-png.flaticon.com/512/11329/11329060.png"
+            max-width="200"
+            class="mb-6"
+          ></v-img>
 
-      <v-divider></v-divider>
-      <div v-if="!items"></div>
-      <div v-else>
-        <div v-if="!items.length"></div>
-        <div v-else>
-          <v-card
-            class="mx-auto"
-            max-width="500"
-            flat
+          <h2 class="text-h5 font-weight-medium mb-2">
+            {{ translate("store-module.cart.empty") }}
+          </h2>
+
+          <p class="text-body-2 text-grey-darken-1 mb-6">
+            {{ translate("store-module.cart.add-empty-state") }}
+          </p>
+
+          <v-btn
+            color="primary"
+            prepend-icon="mdi-cart-plus"
+            @click="closeCartSidebar()"
           >
-            <v-container fluid>
-              <v-row dense>
-                <v-col
-                  cols="12"
-                  class="scroll-container-component" style="max-height: 66.5vh;"
+            {{ translate("store-module.cart.go-store") }}
+          </v-btn>
+        </v-container>
+      </template>
+      <template v-else>
+        <v-card
+          class="mx-auto"
+          max-width="500"
+          flat
+        >
+          <v-container fluid>
+            <v-row dense>
+              <v-col
+                cols="12"
+                class="scroll-container-component" style="max-height: 66.5vh;"
+              >
+                <v-card
+                  v-for="item in items"
+                  :key="item.id"
+                  class="mb-2"
                 >
-                  <v-card
-                    v-for="item in items"
-                    :key="item.id"
-                    class="mb-2"
-                  >
-                    <v-img
-                      :src="item.product.image"
-                      lazy-src="/logo/shadai-main.jpeg"
-                      class="align-end"
-                      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                      height="100px"
-                      cover
-                    ><v-card-title class="text-white" :text="strLimit(item.product.name, 50)"></v-card-title>
-                    </v-img>
-                  
-                    <div class="d-flex align-center justify-center ma-2">
-                      <span class="text-subtitle-1">{{ moneyFormat(item.quantity * item.product.price) }}</span>
-                      <v-spacer></v-spacer>
-                      <div class="pa-2">
-                        <Quantity :item_parent="item"/>
-                      </div>
-                      <RemoveCartItem :parent_item_form="item" />
+                  <v-img
+                    :src="item.product.image"
+                    lazy-src="/logo/shadai-main.jpeg"
+                    class="align-end"
+                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                    height="100px"
+                    cover
+                  ><v-card-title class="text-white" :text="strLimit(item.product.name, 50)"></v-card-title>
+                  </v-img>
+                
+                  <div class="d-flex align-center justify-center ma-2">
+                    <span class="text-subtitle-1">{{ moneyFormat(item.quantity * item.product.price) }}</span>
+                    <v-spacer></v-spacer>
+                    <div class="pa-2">
+                      <Quantity :item_parent="item"/>
                     </div>
-                  </v-card>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card>
+                    <RemoveCartItem :parent_item_form="item" />
+                  </div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
 
-          <v-card flat>
-            <v-card-text>
-              <div class="text-h5"><strong class="text-green">MXN</strong>{{ moneyFormat(total) }}</div>
-              <v-icon
-                icon="mdi-truck"
-                size="large"
-                color="success"
-              ></v-icon>
-              <span class="ml-2 text-h6">{{ moneyFormat(shippingTotal) }}</span>
-            </v-card-text>
-            <div class="d-flex flex-column align-center justify-center ma-2">
-              <v-btn color="success" class="mb-2 shiny-text" block @click="checkout()">{{ translate("checkout") }}</v-btn>
-              <v-btn color="grey-lighten-3" block @click="goToCart()">{{ translate("go-cart") }}</v-btn>
-            </div>
-          </v-card>
-        </div>
-      </div>
-    </v-navigation-drawer>
-  </div>
+        <v-card flat>
+          <v-card-text>
+            <div class="text-h5"><strong class="text-green">MXN</strong>{{ moneyFormat(total) }}</div>
+            <v-icon
+              icon="mdi-truck"
+              size="large"
+              color="success"
+            ></v-icon>
+            <span class="ml-2 text-h6">{{ moneyFormat(shippingTotal) }}</span>
+          </v-card-text>
+          <div class="d-flex flex-column align-center justify-center ma-2">
+            <v-btn color="success" class="mb-2 shiny-text" block @click="checkout()">{{ translate("checkout") }}</v-btn>
+            <v-btn color="grey-lighten-3" block @click="goToCart()">{{ translate("go-cart") }}</v-btn>
+          </div>
+        </v-card>
+      </template>
+    </div>
+  </v-navigation-drawer>
 </template>
 
 <script>
