8b70df01aedc566f3f523dbd0e4b8c7688b19d8d
daniel mendez <danielmenc@outlook.com>
2025-09-21 22:35:32 -0600

    #commit improve buttons to show when the stock is empty from any product

--- src/components/Store/Actions/Items.vue
+++ src/components/Store/Actions/Items.vue
@@ -3,13 +3,15 @@
   <div v-else class="d-flex flex-row-reverse mx-2 px-2 pt-3">
     <v-btn
       :loading="loading"
+      :disabled="stock == 0"
       class="mx-2 text-white"
-      color="light-green"
+      :color="stock == 0 ? 'grey' : 'light-green'"
       density="comfortable"
       @click="buyNow(productId)"
     >{{ translate("buy-now") }} {{ moneyFormat(itemPrice) }}</v-btn>
     <v-btn
       :loading="loading"
+      :disabled="stock == 0"
       color="blue-darken-1"
       variant="tonal"
       density="comfortable"
@@ -36,6 +38,7 @@ export default {
       preventSnackbar: false,
       itemPrice: 0.00,
       productId: null,
+      stock: null,
       form: {}
     }
   },
@@ -43,6 +46,7 @@ export default {
     if (!this.parent_item) return
     this.productId = this.parent_item.id
     this.itemPrice = this.parent_item.price
+    this.stock = this.parent_item.stock
     this.setData()
   },
 }
