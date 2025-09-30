ef46a46b233ad010bb536898d3026040f35eecbb
daniel mendez <danielmenc@outlook.com>
2025-09-29 22:41:54 -0600

    #commit refactor the order summary into a new component

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -5,27 +5,7 @@
         <Items />
       </v-col>
       <v-col cols="4" class="pt-2">
-        <v-card class="pa-2" outlined>
-          <v-card-title class="text-h6">{{ translate("cart-module.order-summary") }}</v-card-title>
-          <v-divider></v-divider>
-          <v-card-text>
-            <div class="d-flex justify-space-between">
-              <span>{{ translate("cart-module.subtotal") }}</span>
-              <span>MXN$ 0.00</span>
-            </div>
-            <div class="d-flex justify-space-between">
-              <span>{{ translate("cart-module.tax") }}</span>
-              <span>MXN$ 0.00</span>
-            </div>
-            <div class="d-flex justify-space-between">
-              <span>{{ translate("cart-module.total") }}</span>
-              <span>MXN$ 0.00</span>
-            </div>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="success" block>{{ translate("cart-module.checkout") }}</v-btn>
-          </v-card-actions>
-        </v-card>
+        <OrderSummary />
       </v-col>
     </v-row>
   </div>
@@ -33,11 +13,13 @@
 <script>
 import Responsive from '@/components/Common/Responsive.vue'
 import Items from '../components/Cart/Items.vue';
+import OrderSummary from '../components/Cart/OrderSummary.vue';
 
 export default {
   extends: Responsive,
   components: {
-    Items
+    Items,
+    OrderSummary
   }
 }
 </script>
